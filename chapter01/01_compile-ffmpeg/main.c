1a9bf9b37f38fd1c1abd8f35564bea28d5cd87bc
DoukheeWon <fain9301@yahoo.com>
2024-09-22 18:34:42 +0900

    feat: makefile add

--- chapter01/01_compile-ffmpeg/main.c
+++ chapter01/01_compile-ffmpeg/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+// FFMPEG Codec
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <inttypes.h>
 
 
 int main(int argc, char **argv) {
+
     return 0;
 }
